4e3f6be17cc5c9a93fa20ba0cd528ec7e23eb698
Aaron Rosenberg <aaron.matthew.rosenberg@gmail.com>
2019-11-06 22:31:59 -0500

    ignores set up as module, switching to dev branch now that main structure is altered

---
+++ ignore.py
@@ -0,0 +1,10 @@
+"""
+Bad way to store credentials but better than hard coded for local deployments.
+
+Look into cloud integrated services for deployment; e.g., AWS Secrets Manager
+"""
+
+twilio = {
+    'ssid': '****',
+    'secret_key': '****',
+}
