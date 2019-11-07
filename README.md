f9ec49b6985660a2bd05cdeaf3d04be5a60b7865
Aaron Rosenberg <aaron.matthew.rosenberg@gmail.com>
2019-11-06 22:48:50 -0500

    refactor and turn into a package

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # sms_app
 
 SMS app for sending out text-based ESM surveys
+
+## Setup
+- `pip install -e .`: creates interactive build of package in local Python environment.
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sms_app',
+    version='0.0.1',
+    packages=find_packages(),
+    author='Josh Rosenberg',
+    author_email='',
+    description='',
+    url='https://github.com/picsul/sms_app',
+)
