8755b8f653e5471d4f43b2ec92d796cd6df1eeeb
Alex Lishinski <alexlishinski@gmail.com>
2019-12-12 15:56:32 -0500

    testing the scheduling module

--- sms_app/scheduling.py
+++ sms_app/scheduling.py
@@ -2,6 +2,7 @@ import schedule
 import time
 
 # will need to import the outgoing message functions here as well
+from .send_sms import outgoing_sms, message_the_list, list_of_numbers
 
 # so we'll have a list of phone numbers, ideally in the database that makes up our list. We also want to make sure that every instance of contacting the same person gets linked up in the database. Will need to mess around with the database model in all likelihood to make sure that we have timestamps and suc
 
@@ -9,11 +10,14 @@ import time
 the_schedule = Scheduler()
 
 # create job instances to specify that we blast out the text to the numbers list
-the_schedule.every().monday.at("14:00").do(message_the_list, body = "Happy Monday, are you available to take the survey?")
-the_schedule.every().tuesday.at("15:00").do(message_the_list)
-the_schedule.every().wednesday.at("14:30").do(message_the_list)
-the_schedule.every().thursday.at("16:00").do(message_the_list)
-the_schedule.every().friday.at("12:00").do(message_the_list)
+#the_schedule.every().monday.at("14:00").do(message_the_list, body = "Happy Monday, are you available to take the survey?")
+#the_schedule.every().tuesday.at("15:00").do(message_the_list)
+#the_schedule.every().wednesday.at("14:30").do(message_the_list)
+#the_schedule.every().thursday.at("16:00").do(message_the_list)
+#the_schedule.every().friday.at("12:00").do(message_the_list)
+
+
+the_schedule.every(1).minute.do(outgoing_sms, body = "Happy Monday, are you available to take the survey?")
 
 # if only want to do each one once, add return schedule.CancelJob to the bottom of the message the list function
 
