72adc4accc7354fb8557a92ddf1bf68dae1e86d0
Alex Lishinski <alexlishinski@gmail.com>
2019-12-13 10:56:07 -0500

    Comment out the schedule code

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from flask_migrate import upgrade as upgrade_database
 from sms_app import app, db, parsers, prepare_app
-from sms_app.scheduling import the_schedule
+#from sms_app.scheduling import the_schedule
 
 prepare_app(environment='development')
 migrate = Migrate(app, db)
@@ -30,12 +30,12 @@ def dbseed():
     with open('survey.json') as survey_file:
         db.save(parsers.survey_from_json(survey_file.read()))
 
-def run_schedule():        
-    while 1:
-        the_schedule.run_pending()
-        time.sleep(1)       
+#def run_schedule():        
+#    while 1:
+#        the_schedule.run_pending()
+#        time.sleep(1)       
         
 if __name__ == "__main__":
-    t = Thread(target=run_schedule)
-    t.start()  
+    #t = Thread(target=run_schedule)
+    #t.start()  
     manager.run()
