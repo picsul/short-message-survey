03fd435ded674afb2d014562d673b6b4510f9de9
Alex Lishinski <alexlishinski@gmail.com>
2019-12-15 16:14:08 -0500

    Create new jobs file

---
+++ jobs.py
@@ -0,0 +1,10 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from sms_app.send_sms import outgoing_sms
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes='1')
+def josh_message():
+	outgoing_sms('+18652361445', 'Come on, man!')
+
+sched.start()
