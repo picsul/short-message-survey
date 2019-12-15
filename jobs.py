b69b1bfaf0207414838e669ca3bceb65ded62c43
Alex Lishinski <alexlishinski@gmail.com>
2019-12-15 16:35:32 -0500

    Comment out recurring code

--- jobs.py
+++ jobs.py
@@ -3,8 +3,8 @@ from sms_app.send_sms import outgoing_sms
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=1)
-def josh_message():
-    outgoing_sms('+18652361445', 'Come on, man!')
+#@sched.scheduled_job('interval', minutes=1)
+#def josh_message():
+#    outgoing_sms('+18652361445', 'Come on, man!')
 
 sched.start()
