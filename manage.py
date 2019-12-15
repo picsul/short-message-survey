12c26baf7d939ed16024fbb7f70bb5e3b8ecb38a
Joshua Rosenberg <jmichaelrosenberg@gmail.com>
2019-12-15 16:33:29 -0500

    remove references to APScheduler

--- manage.py
+++ manage.py
@@ -2,11 +2,6 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from flask_migrate import upgrade as upgrade_database
 from sms_app import app, db, parsers, prepare_app
-#from sms_app.scheduling import the_schedule
-from apscheduler.schedulers.background import BackgroundScheduler
-from sms_app.send_sms import outgoing_sms
-
-from flask_apscheduler import APScheduler
 
 prepare_app(environment='development')
 migrate = Migrate(app, db)
@@ -33,23 +28,7 @@ manager.add_command('db', MigrateCommand)
 def dbseed():
     with open('survey.json') as survey_file:
         db.save(parsers.survey_from_json(survey_file.read()))
-
-#def run_schedule():        
-#    while 1:
-#        the_schedule.run_pending()
-#        time.sleep(1)       
         
 if __name__ == "__main__":
-    #t = Thread(target=run_schedule)
-    #t.start()  
-    #scheduler = BackgroundScheduler()
-    #scheduler.add_job(outgoing_sms, 'interval', minutes=1, args = ['+15172400923', 'Lets see if this works'])
-    #scheduler.start()
-    
-    scheduler = APScheduler()
-    scheduler.init_app(app)
-    scheduler.start()
-    
-    app.apscheduler.add_job(outgoing_sms, 'interval', minutes=1, args = ['+15172400923', 'Lets see if this works'])
 
     manager.run()
