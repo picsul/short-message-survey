567f2e5b758d7e2c38e4b25f610465365f920612
Alex Lishinski <alexlishinski@gmail.com>
2019-12-15 20:37:59 -0500

    update schedule

--- jobs.py
+++ jobs.py
@@ -7,4 +7,12 @@ sched = BlockingScheduler()
 #def josh_message():
 #    outgoing_sms('+18652361445', 'Come on, man!')
 
+@sched.scheduled_job('cron', day_of_week='sun', hour='20', minute='40', timezone='America/New_York')
+def josh_message():
+    outgoing_sms('+18652361445', 'I hope you get this, man!')
+    
+@sched.scheduled_job('cron', day_of_week='sun', hour='20', minute='45', timezone='America/New_York')
+def josh_message():
+    outgoing_sms('+15172400923', 'I hope you get this, man!')
+
 sched.start()
