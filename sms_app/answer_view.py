027a5205f822f18f38b283487933d7f5c7329d68
Alex Lishinski <alexlishinski@gmail.com>
2019-12-16 09:39:04 -0500

    Delete voice stuff

--- sms_app/answer_view.py
+++ sms_app/answer_view.py
@@ -1,9 +1,7 @@
 from . import app, db
 from .models import Question, Answer
 from flask import url_for, request, session
-#from twilio.twiml.voice_response import VoiceResponse
 from twilio.twiml.messaging_response import MessagingResponse
-
 
 @app.route('/answer/<question_id>', methods=['POST'])
 def answer(question_id):
@@ -23,11 +21,6 @@ def answer(question_id):
 def extract_content(question):
     if is_sms_request():
         return request.values['Body']
-#    elif question.kind == Question.TEXT:
-#        return 'Transcription in progress.'
-#    else:
-#        return request.values['Digits']
-
 
 def redirect_twiml(question):
     response = MessagingResponse()
@@ -40,10 +33,6 @@ def goodbye_twiml():
     if is_sms_request():
         response = MessagingResponse()
         response.message("Thank you for answering our survey. Good bye!")
-#    else:
-#        response = VoiceResponse()
-#        response.say("Thank you for answering our survey. Good bye!")
-#        response.hangup()
     if 'question_id' in session:
         del session['question_id']
     return str(response)
@@ -51,15 +40,6 @@ def goodbye_twiml():
 
 def is_sms_request():
     return 'MessageSid' in request.values.keys()
-
-
-#@app.route('/answer/transcription/<question_id>', methods=['POST'])
-#def answer_transcription(question_id):
-#    session_id = request.values['CallSid']
-#    content = request.values['TranscriptionText']
-#    Answer.update_content(session_id, question_id, content)
-#    return ''
-
 
 def session_id():
     return request.values.get('CallSid') or request.values['MessageSid']
