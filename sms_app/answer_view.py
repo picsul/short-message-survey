56b6525ca3571b4980f7a6099ea144540dd38ff4
Alex Lishinski <alexlishinski@gmail.com>
2019-12-16 09:31:54 -0500

    Comment out voice stuff

--- sms_app/answer_view.py
+++ sms_app/answer_view.py
@@ -1,7 +1,7 @@
 from . import app, db
 from .models import Question, Answer
 from flask import url_for, request, session
-from twilio.twiml.voice_response import VoiceResponse
+#from twilio.twiml.voice_response import VoiceResponse
 from twilio.twiml.messaging_response import MessagingResponse
 
 
@@ -23,10 +23,10 @@ def answer(question_id):
 def extract_content(question):
     if is_sms_request():
         return request.values['Body']
-    elif question.kind == Question.TEXT:
-        return 'Transcription in progress.'
-    else:
-        return request.values['Digits']
+#    elif question.kind == Question.TEXT:
+#        return 'Transcription in progress.'
+#    else:
+#        return request.values['Digits']
 
 
 def redirect_twiml(question):
@@ -40,10 +40,10 @@ def goodbye_twiml():
     if is_sms_request():
         response = MessagingResponse()
         response.message("Thank you for answering our survey. Good bye!")
-    else:
-        response = VoiceResponse()
-        response.say("Thank you for answering our survey. Good bye!")
-        response.hangup()
+#    else:
+#        response = VoiceResponse()
+#        response.say("Thank you for answering our survey. Good bye!")
+#        response.hangup()
     if 'question_id' in session:
         del session['question_id']
     return str(response)
@@ -53,12 +53,12 @@ def is_sms_request():
     return 'MessageSid' in request.values.keys()
 
 
-@app.route('/answer/transcription/<question_id>', methods=['POST'])
-def answer_transcription(question_id):
-    session_id = request.values['CallSid']
-    content = request.values['TranscriptionText']
-    Answer.update_content(session_id, question_id, content)
-    return ''
+#@app.route('/answer/transcription/<question_id>', methods=['POST'])
+#def answer_transcription(question_id):
+#    session_id = request.values['CallSid']
+#    content = request.values['TranscriptionText']
+#    Answer.update_content(session_id, question_id, content)
+#    return ''
 
 
 def session_id():
