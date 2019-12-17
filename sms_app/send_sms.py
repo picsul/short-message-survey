e737ef5c43dce98ca016d3d648ac6293208094c6
Alex Lishinski <alexlishinski@gmail.com>
2019-12-17 10:09:26 -0500

    Updated message_the_list
    added origin number argument to reflect the changes to outgoing_sms

--- sms_app/send_sms.py
+++ sms_app/send_sms.py
@@ -5,7 +5,6 @@ from .models import Number
 account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
 auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
 
-
 client = Client(account_sid, auth_token)
 
 def outgoing_sms(number, origin, body):
@@ -18,24 +17,12 @@ def outgoing_sms(number, origin, body):
 
     print(message.sid)
 
-# This is the database query for the questions module, I'll need something like this
-# question = Question.query.get(question_id)
-
-# This is the database Saving code
-#    db.save(Answer(content=extract_content(question),
-#                   question=question,
-#                   session_id=session_id()))
-
-#number = Number.query.get()
-
-
 list_of_numbers = [] 
 
 for i in range(0, Number.query.count()):
     num = Number.query.get(i+1)
     list_of_numbers.append(num.number)
 
-
-def message_the_list(num_list, body):
+def message_the_list(num_list, origin, body):
     for num in num_list:
-        outgoing_sms(num, body)
+        outgoing_sms(num, origin, body)
