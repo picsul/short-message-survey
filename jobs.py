f4e02baa05bfef4a0c3776be3aa78a01ab702f0d
Alex Lishinski <alexlishinski@gmail.com>
2020-01-10 15:47:35 -0500

    update jobs file

--- jobs.py
+++ jobs.py
@@ -3,16 +3,15 @@ from sms_app.send_sms import outgoing_sms, message_the_list, list_of_numbers
 
 sched = BlockingScheduler()
 
-usc_numbers = ['+18652361445', '+12022588130', '+1‪8652361445'‬, '+18186205367']
+usc_numbers = ['+18652361445', '+12022588130', '+18186205367']
 picsul_numbers = ['+15172400923', '+18652361445']
 
 # test jobs
 
-@sched.scheduled_job('cron', day_of_week='fri', hour='13', minute='03', timezone='America/New_York')
+@sched.scheduled_job('cron', day_of_week='fri', hour='15', minute='53', timezone='America/New_York')
 def josh_message():
     message_the_list(picsul_numbers, 'Please complete this short survey related to your teaching and the planning related to your teaching over the past few days: http://bit.ly/sm-math')
    
-    
 @sched.scheduled_job('cron', day_of_week='fri', hour='13', minute='55', timezone='America/New_York')
 def test_demo_1():
     message_the_list(picsul_numbers,'Please complete this short survey related to your teaching and the planning related to your teaching over the past few days: http://bit.ly/sm-math')
