a7b52ce3e7509275b4e056da56c617cf4303f753
Alex Lishinski <alexlishinski@gmail.com>
2020-01-10 09:55:32 -0500

    change message

--- jobs.py
+++ jobs.py
@@ -1,5 +1,5 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-from sms_app.send_sms import outgoing_sms
+from sms_app.send_sms import outgoing_sms, message_the_list, list_of_numbers
 
 sched = BlockingScheduler()
 
@@ -7,9 +7,10 @@ sched = BlockingScheduler()
 #def josh_message():
 #    outgoing_sms('+18652361445', 'Come on, man!')
 
-@sched.scheduled_job('cron', day_of_week='sun', hour='20', minute='40', timezone='America/New_York')
+@sched.scheduled_job('cron', day_of_week='fri', hour='09', minute='59', timezone='America/New_York')
 def josh_message():
-    outgoing_sms('+18652361445', 'I hope you get this, man!')
+    outgoing_sms('+15172400923', 'Please complete this short survey related to your teaching and the planning related to your teaching over the past few days: http://bit.ly/sm-math')
+   
     
 @sched.scheduled_job('cron', day_of_week='sun', hour='20', minute='45', timezone='America/New_York')
 def josh_message():
