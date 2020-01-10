c975362f3574d20252f0b8d103d6acf5f9ca774a
Alex Lishinski <alexlishinski@gmail.com>
2020-01-10 10:07:42 -0500

    update numbers and message

--- jobs.py
+++ jobs.py
@@ -7,13 +7,13 @@ sched = BlockingScheduler()
 #def josh_message():
 #    outgoing_sms('+18652361445', 'Come on, man!')
 
-@sched.scheduled_job('cron', day_of_week='fri', hour='09', minute='59', timezone='America/New_York')
+@sched.scheduled_job('cron', day_of_week='fri', hour='10', minute='11', timezone='America/New_York')
 def josh_message():
     outgoing_sms('+15172400923', 'Please complete this short survey related to your teaching and the planning related to your teaching over the past few days: http://bit.ly/sm-math')
    
     
-@sched.scheduled_job('cron', day_of_week='sun', hour='20', minute='45', timezone='America/New_York')
+@sched.scheduled_job('cron', day_of_week='fri', hour='10', minute='11', timezone='America/New_York')
 def josh_message():
-    outgoing_sms('+15172400923', 'I hope you get this, man!')
+    outgoing_sms('+18652361445','Please complete this short survey related to your teaching and the planning related to your teaching over the past few days: http://bit.ly/sm-math')
 
 sched.start()
