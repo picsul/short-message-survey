b1fa382771ff60b94936e4e4b708c1eb7ecd9f0b
Alex Lishinski <alexlishinski@gmail.com>
2020-01-13 13:40:22 -0500

    add my number to demo

--- jobs.py
+++ jobs.py
@@ -3,7 +3,7 @@ from sms_app.send_sms import outgoing_sms, message_the_list, list_of_numbers
 
 sched = BlockingScheduler()
 
-usc_numbers = ['+18652361445', '+12022588130', '+18186205367', '+18593947313']
+usc_numbers = ['+18652361445', '+12022588130', '+18186205367', '+18593947313', '+15172400923']
 picsul_numbers = ['+15172400923', '+18652361445']
 
 # test jobs
