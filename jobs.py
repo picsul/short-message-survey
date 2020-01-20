74789dd8d96d60fcd84e5aba3dd9837703e31e84
Alex Lishinski <alexlishinski@gmail.com>
2020-01-20 09:39:15 -0500

    Comment out jobs

--- jobs.py
+++ jobs.py
@@ -12,23 +12,23 @@ picsul_numbers = ['+15172400923', '+18652361445']
 def josh_message():
     message_the_list(picsul_numbers, 'Please complete this short survey related to your teaching and the planning related to your teaching over the past few days: http://bit.ly/sm-math')
    
-@sched.scheduled_job('cron', day_of_week='mon', hour='09', minute='19', timezone='America/New_York')
-def test_demo_1():
-    message_the_list(picsul_numbers,'Please complete this short survey related to your teaching and the planning related to your teaching over the past few days: http://bit.ly/sm-math')
+#@sched.scheduled_job('cron', day_of_week='mon', hour='09', minute='19', timezone='America/New_York')
+#def test_demo_1():
+#    message_the_list(picsul_numbers,'Please complete this short survey related to your teaching and the planning related to your teaching over the past few days: http://bit.ly/sm-math')
 
-@sched.scheduled_job('cron', day_of_week='mon', hour='09', minute='35', timezone='America/New_York')
-def test_demo_2():
-    message_the_list(picsul_numbers,'Please complete this short survey related to your teaching and the planning related to your teaching over the past few days: http://bit.ly/sm-math')
+#@sched.scheduled_job('cron', day_of_week='mon', hour='09', minute='35', timezone='America/New_York')
+#def test_demo_2():
+#    message_the_list(picsul_numbers,'Please complete this short survey related to your teaching and the planning related to your teaching over the past few days: http://bit.ly/sm-math')
 
 # demo jobs    
     
-@sched.scheduled_job('cron', day_of_week='mon', hour='13', minute='55', timezone='America/New_York')
-def demo_message_1():
-    message_the_list(usc_numbers, 'Please complete this short survey related to your teaching and the planning related to your teaching over the past few days: http://bit.ly/sm-math')
+#@sched.scheduled_job('cron', day_of_week='mon', hour='13', minute='55', timezone='America/New_York')
+#def demo_message_1():
+#    message_the_list(usc_numbers, 'Please complete this short survey related to your teaching and the planning related to your teaching over the past few days: http://bit.ly/sm-math')
    
-@sched.scheduled_job('cron', day_of_week='mon', hour='14', minute='15', timezone='America/New_York')
-def demo_message_2():
-    message_the_list(usc_numbers, 'Please complete this short survey related to your teaching and the planning related to your teaching over the past few days: http://bit.ly/sm-math')
+#@sched.scheduled_job('cron', day_of_week='mon', hour='14', minute='15', timezone='America/New_York')
+#def demo_message_2():
+#    message_the_list(usc_numbers, 'Please complete this short survey related to your teaching and the planning related to your teaching over the past few days: http://bit.ly/sm-math')
    
     
 sched.start()
