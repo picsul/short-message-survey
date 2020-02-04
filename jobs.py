b1c64ed1406d40665c840813393c1998929d4db7
Alex Lishinski <alexlishinski@gmail.com>
2020-02-04 15:23:09 -0500

    add outgoing SMS test

--- jobs.py
+++ jobs.py
@@ -5,9 +5,12 @@ sched = BlockingScheduler()
 
 usc_numbers = ['+18652361445', '+12022588130', '+18186205367', '+18593947313', '+15172400923']
 picsul_numbers = ['+15172400923', '+18652361445']
+alex_numbers = ['+15172400923']
+
+twilio_numbers = ['+18652369199', '+18652369184']
 
 @sched.scheduled_job('cron', day_of_week='mon', hour='17', minute='00', timezone='US/Eastern')
-def eastern_message():
-    message_the_list(picsul_numbers, "Hey")
+def test_message():
+    message_the_list(alex_numbers, "Hey", twilio_numbers[1])
    
 sched.start()
