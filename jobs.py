c3add7cd062575b16e8739fa108c71de1023206e
Alex Lishinski <alexlishinski@gmail.com>
2020-02-05 00:33:02 -0500

    Add code to pull numbers from database

--- jobs.py
+++ jobs.py
@@ -17,4 +17,71 @@ def test_message():
 def section_query_message():
     message_the_list(list_of_numbers, "Thank you for participating in our CS education study. Please indicate the start time of your COSC 102 lab section by responding with one of the following options: A (12:20), B (1:25), C (2:30), or D (3:35).", twilio_numbers[1])
    
+
+# section times 1220, 125, 230, 335, all 50 minutes long
+# launch times 115 220 325 430
+
+
+# Get the numbers from the database
+
+# Section A
+
+ace = Number.query.filter_by(name = "A").all()
+
+a_numbers = []
+
+for thing in ace:
+    a_numbers.append(thing.number)
+
+# Section B
+    
+bez = Number.query.filter_by(name = "B").all()
+
+b_numbers = []
+
+for thing in bez:
+    b_numbers.append(thing.number)
+    
+# Section C
+
+sez = Number.query.filter_by(name = "C").all()
+
+c_numbers = []
+
+for thing in sez:
+    c_numbers.append(thing.number)
+    
+# Section D
+    
+dez = Number.query.filter_by(name = "D").all()
+
+d_numbers = []
+
+for thing in dez:
+    d_numbers.append(thing.number)
+
+# Initial Prompt message
+prompt = "Are you ready to take the survey?"
+
+# Section 1
+@sched.scheduled_job('cron', day_of_week='wed', hour='13', minute='15', timezone='US/Eastern')
+def section_query_message():
+    message_the_list(a_numbers, prompt, twilio_numbers[0])
+    
+# Section 2
+@sched.scheduled_job('cron', day_of_week='wed', hour='14', minute='20', timezone='US/Eastern')
+def section_query_message():
+    message_the_list(b_numbers, prompt, twilio_numbers[0])
+    
+# Section 3
+@sched.scheduled_job('cron', day_of_week='wed', hour='15', minute='25', timezone='US/Eastern')
+def section_query_message():
+    message_the_list(c_numbers, prompt, twilio_numbers[0])
+
+# Section 4
+@sched.scheduled_job('cron', day_of_week='wed', hour='16', minute='30', timezone='US/Eastern')
+def section_query_message():
+    message_the_list(d_numbers, prompt, twilio_numbers[0])
+
+
 sched.start()
