4f40ee1feffdca42ac46ea4d5f9e3bcccf0b1f45
Alex Lishinski <alexlishinski@gmail.com>
2020-02-09 21:15:55 -0500

    add new links and code
    refactor the code so that it picks out the links for each group from the main list of links and now I don't have to do that manually.

--- jobs.py
+++ jobs.py
@@ -7,30 +7,7 @@ sched = BlockingScheduler()
 usc_numbers = ['+18652361445', '+12022588130', '+18186205367', '+18593947313', '+15172400923']
 picsul_numbers = ['+15172400923', '+18652361445']
 
-# WEEK 2 USC Survey INFO
 
-pacific_links = ["https://usc.qualtrics.com/jfe/form/SV_8nWt5HjaTD9Rlw9?Q_DL=eViuJjZporF0zUp_8nWt5HjaTD9Rlw9_MLRP_2hLHbz5Hyf5NBTn&Q_CHL=gl",
-                 "https://usc.qualtrics.com/jfe/form/SV_8nWt5HjaTD9Rlw9?Q_DL=eViuJjZporF0zUp_8nWt5HjaTD9Rlw9_MLRP_dhePwP0GqDTu5zT&Q_CHL=gl"]
-
-mountain_links = ["https://usc.qualtrics.com/jfe/form/SV_8nWt5HjaTD9Rlw9?Q_DL=eViuJjZporF0zUp_8nWt5HjaTD9Rlw9_MLRP_bENvkQCurQsEEHr&Q_CHL=gl",
-                  "https://usc.qualtrics.com/jfe/form/SV_8nWt5HjaTD9Rlw9?Q_DL=eViuJjZporF0zUp_8nWt5HjaTD9Rlw9_MLRP_7NDerHqFiDoygkZ&Q_CHL=gl",
-                  "https://usc.qualtrics.com/jfe/form/SV_8nWt5HjaTD9Rlw9?Q_DL=eViuJjZporF0zUp_8nWt5HjaTD9Rlw9_MLRP_1yTlsuplG4THHUh&Q_CHL=gl"]
-                  
-central_links = ["https://usc.qualtrics.com/jfe/form/SV_8nWt5HjaTD9Rlw9?Q_DL=eViuJjZporF0zUp_8nWt5HjaTD9Rlw9_MLRP_cMcQoUkQvJ4fPxP&Q_CHL=gl",
-                 "https://usc.qualtrics.com/jfe/form/SV_8nWt5HjaTD9Rlw9?Q_DL=eViuJjZporF0zUp_8nWt5HjaTD9Rlw9_MLRP_9MG6ZcTjGBSZteR&Q_CHL=gl",
-                 "https://usc.qualtrics.com/jfe/form/SV_8nWt5HjaTD9Rlw9?Q_DL=eViuJjZporF0zUp_8nWt5HjaTD9Rlw9_MLRP_1Y2uCGvdVZXBjkp&Q_CHL=gl",
-                 "https://usc.qualtrics.com/jfe/form/SV_8nWt5HjaTD9Rlw9?Q_DL=eViuJjZporF0zUp_8nWt5HjaTD9Rlw9_MLRP_3zcn3K7bOxN3W8R&Q_CHL=gl",
-                 "https://usc.qualtrics.com/jfe/form/SV_8nWt5HjaTD9Rlw9?Q_DL=eViuJjZporF0zUp_8nWt5HjaTD9Rlw9_MLRP_5ijiq8AYRMCiZ7L&Q_CHL=gl",
-                 "https://usc.qualtrics.com/jfe/form/SV_8nWt5HjaTD9Rlw9?Q_DL=eViuJjZporF0zUp_8nWt5HjaTD9Rlw9_MLRP_1B4qLoACivOVvLf&Q_CHL=gl"]
-
-eastern_links = ["https://usc.qualtrics.com/jfe/form/SV_8nWt5HjaTD9Rlw9?Q_DL=eViuJjZporF0zUp_8nWt5HjaTD9Rlw9_MLRP_9GZflsMcnfG5DF3&Q_CHL=gl",
-                 "https://usc.qualtrics.com/jfe/form/SV_8nWt5HjaTD9Rlw9?Q_DL=eViuJjZporF0zUp_8nWt5HjaTD9Rlw9_MLRP_a8CSWgVFh1lXZ7D&Q_CHL=gl",
-                 "https://usc.qualtrics.com/jfe/form/SV_8nWt5HjaTD9Rlw9?Q_DL=eViuJjZporF0zUp_8nWt5HjaTD9Rlw9_MLRP_7a1R5dSFJSeS073&Q_CHL=gl",
-                 "https://usc.qualtrics.com/jfe/form/SV_8nWt5HjaTD9Rlw9?Q_DL=eViuJjZporF0zUp_8nWt5HjaTD9Rlw9_MLRP_cCo86CmUBi2yIh7&Q_CHL=gl",
-                 "https://usc.qualtrics.com/jfe/form/SV_8nWt5HjaTD9Rlw9?Q_DL=eViuJjZporF0zUp_8nWt5HjaTD9Rlw9_MLRP_bei1a3O9UCJcZw1&Q_CHL=gl",
-                 "https://usc.qualtrics.com/jfe/form/SV_8nWt5HjaTD9Rlw9?Q_DL=eViuJjZporF0zUp_8nWt5HjaTD9Rlw9_MLRP_86M4CaqRe3mXAd7&Q_CHL=gl",
-                 "https://usc.qualtrics.com/jfe/form/SV_8nWt5HjaTD9Rlw9?Q_DL=eViuJjZporF0zUp_8nWt5HjaTD9Rlw9_MLRP_bDgxYnWNiNzGonP&Q_CHL=gl",
-                 "https://usc.qualtrics.com/jfe/form/SV_8nWt5HjaTD9Rlw9?Q_DL=eViuJjZporF0zUp_8nWt5HjaTD9Rlw9_MLRP_8iulysbXuJYk2GN&Q_CHL=gl"]
 
 # test jobs
 #names = ['Alex', 'Josh']
@@ -55,7 +32,108 @@ eastern_links = ["https://usc.qualtrics.com/jfe/form/SV_8nWt5HjaTD9Rlw9?Q_DL=eVi
 #def test_message_2():
 #    message_the_list(usc_numbers, "You should receive 4 test messages over the next 4 hours, starting in 15 minutes, let me know if you don't, and no need to click the links.")    
 
-test_links = ["www.google.com"]
+test_lintimezones = [
+'US/Pacific',
+'US/Eastern',
+'US/Central',
+'US/Central',
+'US/Eastern',
+'US/Eastern',
+'US/Eastern',
+'US/Pacific',
+'US/Central',
+'US/Central',
+'US/Eastern',
+'US/Eastern',
+'US/Mountain',
+'US/Mountain',
+'US/Eastern',
+'US/Eastern',
+'US/Central',
+'US/Mountain',
+'US/Central']
+
+links_w3_mon = ['https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_2hLHbz5Hyf5NBTn&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_9GZflsMcnfG5DF3&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_cMcQoUkQvJ4fPxP&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_9MG6ZcTjGBSZteR&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_a8CSWgVFh1lXZ7D&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_7a1R5dSFJSeS073&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_cCo86CmUBi2yIh7&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_dhePwP0GqDTu5zT&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_1Y2uCGvdVZXBjkp&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_3zcn3K7bOxN3W8R&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_bei1a3O9UCJcZw1&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_86M4CaqRe3mXAd7&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_bENvkQCurQsEEHr&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_7NDerHqFiDoygkZ&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_bDgxYnWNiNzGonP&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_8iulysbXuJYk2GN&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_5ijiq8AYRMCiZ7L&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_1yTlsuplG4THHUh&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_1B4qLoACivOVvLf&Q_CHL=gl']
+
+links_w3_fri = ['https://usc.qualtrics.com/jfe/form/SV_5ow49zzrj58C5EN?Q_DL=bskVU860dqVtro1_5ow49zzrj58C5EN_MLRP_2hLHbz5Hyf5NBTn&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_5ow49zzrj58C5EN?Q_DL=bskVU860dqVtro1_5ow49zzrj58C5EN_MLRP_9GZflsMcnfG5DF3&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_5ow49zzrj58C5EN?Q_DL=bskVU860dqVtro1_5ow49zzrj58C5EN_MLRP_cMcQoUkQvJ4fPxP&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_5ow49zzrj58C5EN?Q_DL=bskVU860dqVtro1_5ow49zzrj58C5EN_MLRP_9MG6ZcTjGBSZteR&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_5ow49zzrj58C5EN?Q_DL=bskVU860dqVtro1_5ow49zzrj58C5EN_MLRP_a8CSWgVFh1lXZ7D&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_5ow49zzrj58C5EN?Q_DL=bskVU860dqVtro1_5ow49zzrj58C5EN_MLRP_7a1R5dSFJSeS073&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_5ow49zzrj58C5EN?Q_DL=bskVU860dqVtro1_5ow49zzrj58C5EN_MLRP_cCo86CmUBi2yIh7&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_5ow49zzrj58C5EN?Q_DL=bskVU860dqVtro1_5ow49zzrj58C5EN_MLRP_dhePwP0GqDTu5zT&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_5ow49zzrj58C5EN?Q_DL=bskVU860dqVtro1_5ow49zzrj58C5EN_MLRP_1Y2uCGvdVZXBjkp&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_5ow49zzrj58C5EN?Q_DL=bskVU860dqVtro1_5ow49zzrj58C5EN_MLRP_3zcn3K7bOxN3W8R&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_5ow49zzrj58C5EN?Q_DL=bskVU860dqVtro1_5ow49zzrj58C5EN_MLRP_bei1a3O9UCJcZw1&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_5ow49zzrj58C5EN?Q_DL=bskVU860dqVtro1_5ow49zzrj58C5EN_MLRP_86M4CaqRe3mXAd7&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_5ow49zzrj58C5EN?Q_DL=bskVU860dqVtro1_5ow49zzrj58C5EN_MLRP_bENvkQCurQsEEHr&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_5ow49zzrj58C5EN?Q_DL=bskVU860dqVtro1_5ow49zzrj58C5EN_MLRP_7NDerHqFiDoygkZ&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_5ow49zzrj58C5EN?Q_DL=bskVU860dqVtro1_5ow49zzrj58C5EN_MLRP_bDgxYnWNiNzGonP&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_5ow49zzrj58C5EN?Q_DL=bskVU860dqVtro1_5ow49zzrj58C5EN_MLRP_8iulysbXuJYk2GN&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_5ow49zzrj58C5EN?Q_DL=bskVU860dqVtro1_5ow49zzrj58C5EN_MLRP_5ijiq8AYRMCiZ7L&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_5ow49zzrj58C5EN?Q_DL=bskVU860dqVtro1_5ow49zzrj58C5EN_MLRP_1yTlsuplG4THHUh&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_5ow49zzrj58C5EN?Q_DL=bskVU860dqVtro1_5ow49zzrj58C5EN_MLRP_1B4qLoACivOVvLf&Q_CHL=gl']
+
+links_w4_mon = ['https://usc.qualtrics.com/jfe/form/SV_br2By2MRNJR6zJP?Q_DL=6r0UOQ8cEm3GYwl_br2By2MRNJR6zJP_MLRP_2hLHbz5Hyf5NBTn&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_br2By2MRNJR6zJP?Q_DL=6r0UOQ8cEm3GYwl_br2By2MRNJR6zJP_MLRP_9GZflsMcnfG5DF3&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_br2By2MRNJR6zJP?Q_DL=6r0UOQ8cEm3GYwl_br2By2MRNJR6zJP_MLRP_cMcQoUkQvJ4fPxP&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_br2By2MRNJR6zJP?Q_DL=6r0UOQ8cEm3GYwl_br2By2MRNJR6zJP_MLRP_9MG6ZcTjGBSZteR&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_br2By2MRNJR6zJP?Q_DL=6r0UOQ8cEm3GYwl_br2By2MRNJR6zJP_MLRP_a8CSWgVFh1lXZ7D&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_br2By2MRNJR6zJP?Q_DL=6r0UOQ8cEm3GYwl_br2By2MRNJR6zJP_MLRP_7a1R5dSFJSeS073&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_br2By2MRNJR6zJP?Q_DL=6r0UOQ8cEm3GYwl_br2By2MRNJR6zJP_MLRP_cCo86CmUBi2yIh7&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_br2By2MRNJR6zJP?Q_DL=6r0UOQ8cEm3GYwl_br2By2MRNJR6zJP_MLRP_dhePwP0GqDTu5zT&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_br2By2MRNJR6zJP?Q_DL=6r0UOQ8cEm3GYwl_br2By2MRNJR6zJP_MLRP_1Y2uCGvdVZXBjkp&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_br2By2MRNJR6zJP?Q_DL=6r0UOQ8cEm3GYwl_br2By2MRNJR6zJP_MLRP_3zcn3K7bOxN3W8R&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_br2By2MRNJR6zJP?Q_DL=6r0UOQ8cEm3GYwl_br2By2MRNJR6zJP_MLRP_bei1a3O9UCJcZw1&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_br2By2MRNJR6zJP?Q_DL=6r0UOQ8cEm3GYwl_br2By2MRNJR6zJP_MLRP_86M4CaqRe3mXAd7&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_br2By2MRNJR6zJP?Q_DL=6r0UOQ8cEm3GYwl_br2By2MRNJR6zJP_MLRP_bENvkQCurQsEEHr&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_br2By2MRNJR6zJP?Q_DL=6r0UOQ8cEm3GYwl_br2By2MRNJR6zJP_MLRP_7NDerHqFiDoygkZ&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_br2By2MRNJR6zJP?Q_DL=6r0UOQ8cEm3GYwl_br2By2MRNJR6zJP_MLRP_bDgxYnWNiNzGonP&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_br2By2MRNJR6zJP?Q_DL=6r0UOQ8cEm3GYwl_br2By2MRNJR6zJP_MLRP_8iulysbXuJYk2GN&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_br2By2MRNJR6zJP?Q_DL=6r0UOQ8cEm3GYwl_br2By2MRNJR6zJP_MLRP_5ijiq8AYRMCiZ7L&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_br2By2MRNJR6zJP?Q_DL=6r0UOQ8cEm3GYwl_br2By2MRNJR6zJP_MLRP_1yTlsuplG4THHUh&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_br2By2MRNJR6zJP?Q_DL=6r0UOQ8cEm3GYwl_br2By2MRNJR6zJP_MLRP_1B4qLoACivOVvLf&Q_CHL=gl']
+
+links_w4_fri = ['https://usc.qualtrics.com/jfe/form/SV_eVdRktZlwZTCcPX?Q_DL=2lTdCe7RGfx5Op7_eVdRktZlwZTCcPX_MLRP_2hLHbz5Hyf5NBTn&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eVdRktZlwZTCcPX?Q_DL=2lTdCe7RGfx5Op7_eVdRktZlwZTCcPX_MLRP_9GZflsMcnfG5DF3&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eVdRktZlwZTCcPX?Q_DL=2lTdCe7RGfx5Op7_eVdRktZlwZTCcPX_MLRP_cMcQoUkQvJ4fPxP&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eVdRktZlwZTCcPX?Q_DL=2lTdCe7RGfx5Op7_eVdRktZlwZTCcPX_MLRP_9MG6ZcTjGBSZteR&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eVdRktZlwZTCcPX?Q_DL=2lTdCe7RGfx5Op7_eVdRktZlwZTCcPX_MLRP_a8CSWgVFh1lXZ7D&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eVdRktZlwZTCcPX?Q_DL=2lTdCe7RGfx5Op7_eVdRktZlwZTCcPX_MLRP_7a1R5dSFJSeS073&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eVdRktZlwZTCcPX?Q_DL=2lTdCe7RGfx5Op7_eVdRktZlwZTCcPX_MLRP_cCo86CmUBi2yIh7&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eVdRktZlwZTCcPX?Q_DL=2lTdCe7RGfx5Op7_eVdRktZlwZTCcPX_MLRP_dhePwP0GqDTu5zT&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eVdRktZlwZTCcPX?Q_DL=2lTdCe7RGfx5Op7_eVdRktZlwZTCcPX_MLRP_1Y2uCGvdVZXBjkp&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eVdRktZlwZTCcPX?Q_DL=2lTdCe7RGfx5Op7_eVdRktZlwZTCcPX_MLRP_3zcn3K7bOxN3W8R&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eVdRktZlwZTCcPX?Q_DL=2lTdCe7RGfx5Op7_eVdRktZlwZTCcPX_MLRP_bei1a3O9UCJcZw1&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eVdRktZlwZTCcPX?Q_DL=2lTdCe7RGfx5Op7_eVdRktZlwZTCcPX_MLRP_86M4CaqRe3mXAd7&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eVdRktZlwZTCcPX?Q_DL=2lTdCe7RGfx5Op7_eVdRktZlwZTCcPX_MLRP_bENvkQCurQsEEHr&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eVdRktZlwZTCcPX?Q_DL=2lTdCe7RGfx5Op7_eVdRktZlwZTCcPX_MLRP_7NDerHqFiDoygkZ&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eVdRktZlwZTCcPX?Q_DL=2lTdCe7RGfx5Op7_eVdRktZlwZTCcPX_MLRP_bDgxYnWNiNzGonP&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eVdRktZlwZTCcPX?Q_DL=2lTdCe7RGfx5Op7_eVdRktZlwZTCcPX_MLRP_8iulysbXuJYk2GN&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eVdRktZlwZTCcPX?Q_DL=2lTdCe7RGfx5Op7_eVdRktZlwZTCcPX_MLRP_5ijiq8AYRMCiZ7L&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eVdRktZlwZTCcPX?Q_DL=2lTdCe7RGfx5Op7_eVdRktZlwZTCcPX_MLRP_1yTlsuplG4THHUh&Q_CHL=gl',
+'https://usc.qualtrics.com/jfe/form/SV_eVdRktZlwZTCcPX?Q_DL=2lTdCe7RGfx5Op7_eVdRktZlwZTCcPX_MLRP_1B4qLoACivOVvLf&Q_CHL=gl']
+
+ks = ["www.google.com"]
 
 @sched.scheduled_job('cron', day_of_week='thu', hour='15', minute='03', timezone='US/Eastern')
 def test_message_2():
@@ -80,6 +158,9 @@ def eastern_message():
     static = "Please complete this short survey related to your recent teaching and planning: "
 
     comb_message = []
+    
+    eastern_indices = [i for i, time in enumerate(timezones) if time == 'US/Eastern']
+    eastern_links = [links_w3_mon[i] for i in eastern_indices]
 
     links = eastern_links
 
@@ -96,6 +177,9 @@ def central_message():
     static = "Please complete this short survey related to your recent teaching and planning: "
 
     comb_message = []
+    
+    central_indices = [i for i, time in enumerate(timezones) if time == 'US/Central']
+    central_links = [links_w3_mon[i] for i in central_indices]
 
     links = central_links
 
@@ -112,6 +196,9 @@ def mountain_message():
     static = "Please complete this short survey related to your recent teaching and planning: "
 
     comb_message = []
+    
+    mountain_indices = [i for i, time in enumerate(timezones) if time == 'US/Mountain']
+    mountain_links = [links_w3_mon[i] for i in mountain_indices]
 
     links = mountain_links
 
@@ -128,6 +215,9 @@ def pacific_message():
     static = "Please complete this short survey related to your recent teaching and planning: "
 
     comb_message = []
+    
+    pacific_indices = [i for i, time in enumerate(timezones) if time == 'US/Pacific']
+    pacific_links = [links_w3_mon[i] for i in pacific_indices]
 
     links = pacific_links
 
