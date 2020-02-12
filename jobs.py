3b7805e1db1f2eaa17f7ca3ede8b78b462180eaf
Alex Lishinski <alexlishinski@gmail.com>
2020-02-12 13:37:28 -0500

    update for friday

--- jobs.py
+++ jobs.py
@@ -7,31 +7,6 @@ sched = BlockingScheduler()
 usc_numbers = ['+18652361445', '+12022588130', '+18186205367', '+18593947313', '+15172400923']
 picsul_numbers = ['+15172400923', '+18652361445']
 
-
-
-# test jobs
-#names = ['Alex', 'Josh']
-
-#test_links = ["https://usc-survey.herokuapp.com/", "https://hunter-college-survey.herokuapp.com/"]
-
-#@sched.scheduled_job('cron', day_of_week='thu', hour='15', minute='38', timezone='America/New_York')
-#def test_message():
-#    static = " should get link "
-
-#    comb_message = []
-
-#    links = test_links
-
-#    for name, link in zip(names, links):
-#        comb_message.append(name + static + link)
-
-#    message_the_list_unique(picsul_numbers, comb_message)
-
-    
-#@sched.scheduled_job('cron', day_of_week='thu', hour='16', minute='45', timezone='US/Eastern')
-#def test_message_2():
-#    message_the_list(usc_numbers, "You should receive 4 test messages over the next 4 hours, starting in 15 minutes, let me know if you don't, and no need to click the links.")    
-
 timezones = [
 'US/Pacific',
 'US/Eastern',
@@ -52,26 +27,6 @@ timezones = [
 'US/Central',
 'US/Mountain',
 'US/Central']
-
-links_w3_mon = ['https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_2hLHbz5Hyf5NBTn&Q_CHL=gl',
-'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_9GZflsMcnfG5DF3&Q_CHL=gl',
-'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_cMcQoUkQvJ4fPxP&Q_CHL=gl',
-'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_9MG6ZcTjGBSZteR&Q_CHL=gl',
-'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_a8CSWgVFh1lXZ7D&Q_CHL=gl',
-'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_7a1R5dSFJSeS073&Q_CHL=gl',
-'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_cCo86CmUBi2yIh7&Q_CHL=gl',
-'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_dhePwP0GqDTu5zT&Q_CHL=gl',
-'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_1Y2uCGvdVZXBjkp&Q_CHL=gl',
-'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_3zcn3K7bOxN3W8R&Q_CHL=gl',
-'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_bei1a3O9UCJcZw1&Q_CHL=gl',
-'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_86M4CaqRe3mXAd7&Q_CHL=gl',
-'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_bENvkQCurQsEEHr&Q_CHL=gl',
-'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_7NDerHqFiDoygkZ&Q_CHL=gl',
-'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_bDgxYnWNiNzGonP&Q_CHL=gl',
-'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_8iulysbXuJYk2GN&Q_CHL=gl',
-'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_5ijiq8AYRMCiZ7L&Q_CHL=gl',
-'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_1yTlsuplG4THHUh&Q_CHL=gl',
-'https://usc.qualtrics.com/jfe/form/SV_eaJjdOOvNndgV7v?Q_DL=8l8fHWzs0Tgo7uB_eaJjdOOvNndgV7v_MLRP_1B4qLoACivOVvLf&Q_CHL=gl']
 
 links_w3_fri = ['https://usc.qualtrics.com/jfe/form/SV_5ow49zzrj58C5EN?Q_DL=bskVU860dqVtro1_5ow49zzrj58C5EN_MLRP_2hLHbz5Hyf5NBTn&Q_CHL=gl',
 'https://usc.qualtrics.com/jfe/form/SV_5ow49zzrj58C5EN?Q_DL=bskVU860dqVtro1_5ow49zzrj58C5EN_MLRP_9GZflsMcnfG5DF3&Q_CHL=gl',
@@ -133,8 +88,6 @@ links_w4_fri = ['https://usc.qualtrics.com/jfe/form/SV_eVdRktZlwZTCcPX?Q_DL=2lTd
 'https://usc.qualtrics.com/jfe/form/SV_eVdRktZlwZTCcPX?Q_DL=2lTdCe7RGfx5Op7_eVdRktZlwZTCcPX_MLRP_1yTlsuplG4THHUh&Q_CHL=gl',
 'https://usc.qualtrics.com/jfe/form/SV_eVdRktZlwZTCcPX?Q_DL=2lTdCe7RGfx5Op7_eVdRktZlwZTCcPX_MLRP_1B4qLoACivOVvLf&Q_CHL=gl']
 
-ks = ["www.google.com"]
-
 @sched.scheduled_job('cron', day_of_week='thu', hour='15', minute='03', timezone='US/Eastern')
 def test_message_2():
     static = "Hi, the link is: "
@@ -153,14 +106,14 @@ def test_message_2():
 ### SURVEY MESSAGE JOBS
 
 # Eastern time people
-@sched.scheduled_job('cron', day_of_week='tue', hour='17', minute='00', timezone='US/Eastern')
+@sched.scheduled_job('cron', day_of_week='fri', hour='17', minute='00', timezone='US/Eastern')
 def eastern_message():
     static = "Please complete this short survey related to your recent teaching and planning: "
 
     comb_message = []
     
     eastern_indices = [i for i, time in enumerate(timezones) if time == 'US/Eastern']
-    eastern_links = [links_w3_mon[i] for i in eastern_indices]
+    eastern_links = [links_w3_fri[i] for i in eastern_indices]
 
     links = eastern_links
 
@@ -172,14 +125,14 @@ def eastern_message():
     message_the_list_unique(eastern_numbers, comb_message)
    
 # Central time people
-@sched.scheduled_job('cron', day_of_week='tue', hour='17', minute='00', timezone='US/Central')
+@sched.scheduled_job('cron', day_of_week='fri', hour='17', minute='00', timezone='US/Central')
 def central_message():
     static = "Please complete this short survey related to your recent teaching and planning: "
 
     comb_message = []
     
     central_indices = [i for i, time in enumerate(timezones) if time == 'US/Central']
-    central_links = [links_w3_mon[i] for i in central_indices]
+    central_links = [links_w3_fri[i] for i in central_indices]
 
     links = central_links
 
@@ -191,14 +144,14 @@ def central_message():
     message_the_list_unique(central_numbers, comb_message)
     
 # Mountain time people
-@sched.scheduled_job('cron', day_of_week='tue', hour='17', minute='00', timezone='US/Mountain')
+@sched.scheduled_job('cron', day_of_week='fri', hour='17', minute='00', timezone='US/Mountain')
 def mountain_message():
     static = "Please complete this short survey related to your recent teaching and planning: "
 
     comb_message = []
     
     mountain_indices = [i for i, time in enumerate(timezones) if time == 'US/Mountain']
-    mountain_links = [links_w3_mon[i] for i in mountain_indices]
+    mountain_links = [links_w3_fri[i] for i in mountain_indices]
 
     links = mountain_links
 
@@ -210,14 +163,14 @@ def mountain_message():
     message_the_list_unique(mountain_numbers, comb_message)
 
 # Pacific time people
-@sched.scheduled_job('cron', day_of_week='tue', hour='17', minute='00', timezone='US/Pacific')
+@sched.scheduled_job('cron', day_of_week='fri', hour='17', minute='00', timezone='US/Pacific')
 def pacific_message():
     static = "Please complete this short survey related to your recent teaching and planning: "
 
     comb_message = []
     
     pacific_indices = [i for i, time in enumerate(timezones) if time == 'US/Pacific']
-    pacific_links = [links_w3_mon[i] for i in pacific_indices]
+    pacific_links = [links_w3_fri[i] for i in pacific_indices]
 
     links = pacific_links
 
