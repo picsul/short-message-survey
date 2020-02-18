67da2f8b89b121868cced2d9430dcc2a09b23514
Alex Lishinski <alexlishinski@gmail.com>
2020-02-17 21:21:51 -0500

    comment out so can reinitialize db

--- sms_app/send_sms.py
+++ sms_app/send_sms.py
@@ -20,12 +20,12 @@ def outgoing_sms(number, origin, body):
 
 list_of_numbers = [] 
 
-for i in range(0, Number.query.count()):
-    try:
-        num = Number.query.get(i+1)
-        list_of_numbers.append(num.number)
-    except AttributeError:
-        pass
+#for i in range(0, Number.query.count()):
+#    try:
+#        num = Number.query.get(i+1)
+#        list_of_numbers.append(num.number)
+#    except AttributeError:
+#        pass
     
 def message_the_list(num_list, origin, body):
     for num in num_list:
