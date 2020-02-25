15c6e0711d7853fb991e777fc150b1b72aa6ec5b
Alex Lishinski <alexlishinski@gmail.com>
2020-02-24 23:40:00 -0500

    No tests

--- jobs.py
+++ jobs.py
@@ -29,16 +29,16 @@ student_numbers = []
 for student in students:
     student_numbers.append(student.number)
     
-print(student_numbers)
+#print(student_numbers)
 
 ### Tests
-@sched.scheduled_job('cron', day_of_week='tue', hour='08', minute='00', timezone='America/New_York')
-def test_message_pre():
-    message_the_list(test_numbers, '+19179949576', pre_message)
+#@sched.scheduled_job('cron', day_of_week='tue', hour='08', minute='00', timezone='America/New_York')
+#def test_message_pre():
+#    message_the_list(test_numbers, '+19179949576', pre_message)
     
-@sched.scheduled_job('cron', day_of_week='tue', hour='08', minute='15', timezone='America/New_York')
-def test_message_post():
-    message_the_list(test_numbers, '+19179949576', post_message)       
+#@sched.scheduled_job('cron', day_of_week='tue', hour='08', minute='15', timezone='America/New_York')
+#def test_message_post():
+#    message_the_list(test_numbers, '+19179949576', post_message)       
   
 ### Real Messages
 
