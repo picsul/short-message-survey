a7cc12edfc4c56f97c8efb64aae1da362bc0a233
Alex Lishinski <alexlishinski@gmail.com>
2020-04-29 12:34:01 -0400

    Turn off annoying sqlalchemy thing

--- sms_app/__init__.py
+++ sms_app/__init__.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
