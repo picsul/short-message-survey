77a01ddca118437a03977996b6c0eb93df8e96b5
Alex Lishinski <alexlishinski@gmail.com>
2020-05-18 14:47:57 -0400

    Update the message

--- jobs.py
+++ jobs.py
@@ -7,11 +7,12 @@ sched = BlockingScheduler()
 twilio_numbers = ['+18652639199', '+18652639184']
 
 # post-survey reminder
-nums = [number.number for number in Number.query.all()]
+#nums = [number.number for number in Number.query.all()]
+nums = ["+19313063791", "+13018362777", "+19012828292", "+14233550342", "+16158154541", "+18653820776", "+18656221002", "+14073104075", "+18656077555", "+18655676872", "+16158814866"]
 
-message = "Thank you so much for participating in our COSC 102 study this semester! If you haven't already, we'd like to ask you one last time to please complete the end of the semester survey: https://bit.ly/cosc-102-ps. Your response is very valuable to us and greatly appreciated."
+message = "Thanks again for participating in the COSC 102 study. Please provide your UTK NetID so that we can process your participant payment. Thanks!"
 
-@sched.scheduled_job('cron', day_of_week='mon', hour='11', minute='00', timezone='US/Eastern')
+@sched.scheduled_job('cron', day_of_week='mon', hour='14', minute='55', timezone='US/Eastern')
 def post_reminder():
     message_the_list(nums, message, twilio_numbers[0])
 
