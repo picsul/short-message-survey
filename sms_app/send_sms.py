19d4d7611a693795e1ef455739a309e4ee6f9e1a
Alex Lishinski <alexlishinski@gmail.com>
2020-08-17 20:16:05 -0400

    Delete unneeded code

--- sms_app/send_sms.py
+++ sms_app/send_sms.py
@@ -6,9 +6,6 @@ from .models import Number
 account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
 auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
 
-survey_prompt = "Please respond to this text to begin the survey"
-picsul_number = "+18652639184"
-
 client = Client(account_sid, auth_token)
 
 def outgoing_sms(number, body, out_num):
@@ -21,10 +18,6 @@ def outgoing_sms(number, body, out_num):
 
     print(message.sid)
     
-    
-#'+18652639184' Static message #
-#'+18652639199' survey loop #
-
 list_of_numbers = [] 
 
 for i in range(0, Number.query.count()):
@@ -33,10 +26,6 @@ for i in range(0, Number.query.count()):
         list_of_numbers.append(num.number)
     except AttributeError:
         pass
-
-#def message_the_list(num_list, body, from_num):
-#    for num in num_list:
-#        outgoing_sms(num, body, from_num)
         
 def message_the_list(num_list, body, from_num):
     for num in num_list:
@@ -53,11 +42,6 @@ def parse_email(message, assignment):
     if 'notifications@instructure.com' in message['from']:
         subject = message['subject']
         if assignment in subject:
-            #name = message['from'].split("<")[0].strip(' "')
-            #number = Number.query.filter_by(name = name).first()
-            #outgoing_sms(number.number, survey_prompt, picsul_number)
-            #mail.store(i, '+X-GM-LABELS', '\\Trash')
-            #mail.expunge()
             return True
         else:
             pass
