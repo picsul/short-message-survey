a400bf91a096df83ee295e61edd22ce3f9df31f0
Alex Lishinski <alexlishinski@gmail.com>
2020-08-25 21:26:16 -0400

    remove email reading code

--- jobs.py
+++ jobs.py
@@ -1,5 +1,6 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from sms_app.send_sms import outgoing_sms, message_the_list, list_of_numbers, message_the_list_unique, parse_email
+from sms_app.scan_email import read_email_from_gmail, survey_prompt, picsul_number
 from sms_app.models import Number
 import twilio
 import smtplib
@@ -16,48 +17,6 @@ SMTP_PORT   = 993
 
 sched = BlockingScheduler()
 
-survey_prompt = "Ready to take the COSC 102 / 111 survey? Please respond to this message when you are ready to begin. You will have 5 minutes to complete the survey once you begin, but the survey should take less than a minute."
-picsul_number = os.environ.get("TWILIO_NUMBER_1")
-
-def read_email_from_gmail(assignment):
-    try:
-        mail = imaplib.IMAP4_SSL(SMTP_SERVER)
-        mail.login(FROM_EMAIL,FROM_PWD)
-        mail.select('inbox')
-
-        type, data = mail.search(None, 'ALL')
-        mail_ids = data[0]
-
-        id_list = mail_ids.split()
-        id_list = list(reversed(id_list))
-        for i in id_list[0:10]:
-            typ, data = mail.fetch(i, '(RFC822)' )
-
-            for response_part in data:
-                if isinstance(response_part, tuple):
-                    msg = email.message_from_string(response_part[1].decode('utf-8'))
-                    if parse_email(msg, assignment):
-                        name = msg['from'].split("<")[0].strip(' "')
-                        number = Number.query.filter_by(name = name).first()
-                        try:
-                            outgoing_sms(number.number, survey_prompt, picsul_number)
-                            mail.store(i, '+X-GM-LABELS', '\\Trash')
-                            mail.expunge()
-                        except twilio.base.exceptions.TwilioRestException:
-                            pass 
-                        # to deal with non participating studnets that arent found in the database
-                        except AttributeError:
-                            pass
-                    email_subject = msg['subject']
-                    email_from = msg['from']
-                    print('From : ' + email_from + '\n')
-                    print('Subject : ' + email_subject + '\n')
-                    
-        mail.logout()
-            
-    except Exception as e:
-        print(str(e))
-    
 @sched.scheduled_job('interval', id='cosc102_lab1a', minutes=1)
 def cosc102_lab1a():
     read_email_from_gmail("Lab 1a")
@@ -69,7 +28,6 @@ def cosc102_lab1a():
 #for student in students:
 #    student_numbers.append(student.number)
     
-
 #@sched.scheduled_job('cron', day_of_week='mon', hour='14', minute='55', timezone='US/Eastern')
 #def post_reminder():
 #    message_the_list(student_numbers, message, picsul_number)
