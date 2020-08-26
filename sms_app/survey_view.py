6e5e9687328e61c7a49b8264e5a66063dcbcb417
Alex Lishinski <alexlishinski@gmail.com>
2020-08-25 21:53:49 -0400

    Update survey_view.py

--- sms_app/survey_view.py
+++ sms_app/survey_view.py
@@ -3,6 +3,7 @@ from .models import Survey
 from flask import url_for, session, request
 from twilio.twiml.messaging_response import MessagingResponse
 from sms_app.send_sms import client
+from sms_app.scan_email import survey_prompt
 import datetime
 
 @app.route('/message')
@@ -35,7 +36,7 @@ def sms_survey():
     
     # new code with 5 minute time limit, and reprompt reset
 
-    if message_text == "Are you ready to take the COSC 102 survey? Please respond when you are ready to begin. You will have 5 minutes to complete the survey once you begin, but the survey should only take 1-2 minutes.":
+    if message_text == survey_prompt:
         if 'question_id' in session:
             del session['question_id']
         if 'start_time' in session:
@@ -68,7 +69,7 @@ def redirect_to_first_question(response, survey):
 
 def welcome_user(survey, send_function):
     #welcome_text = 'Welcome to the %s' % survey.title
-    welcome_text = 'Please indicate your agreement with the following statements with respect to your last COSC 102 class on a 1 - 5 scale, with 1 indicating strong disagreement, 3 indicating that you neither agree nor disagree, and 5 indicating strong agreement.' 
+    welcome_text = 'Please indicate your agreement right now with the following statements with respect to your COSC 102 / 111 class on a 1 - 5 scale, with 1 indicating strong disagreement, 3 indicating that you neither agree nor disagree, and 5 indicating strong agreement.' 
     send_function(welcome_text)
 
 def survey_error(survey, send_function):
