9e41a844c1c4f646b1de0c6e6584f8b5d1df4e91
Alex Lishinski <alexlishinski@gmail.com>
2020-08-27 14:08:34 -0400

    Update survey_view.py

--- sms_app/survey_view.py
+++ sms_app/survey_view.py
@@ -22,6 +22,7 @@ def sms_survey():
     
     print(type(messages))
     print(messages)
+    print(messages[0].sid)
     # old code
     #survey = Survey.query.first()
     #if survey_error(survey, response.message):
