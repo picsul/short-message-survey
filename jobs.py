bbb54dc1fd5c15dfd012fb57da1a056339594cf1
Alex Lishinski <alexlishinski@gmail.com>
2021-01-11 23:32:59 -0500

    Update jobs.py

--- jobs.py
+++ jobs.py
@@ -34,39 +34,44 @@ l6 = ["Lab 6"]
 exam = ["Final Exam"]
 
 ## One job   
-@sched.scheduled_job('date', id='cosc_timed_1', run_date='2021-01-11 23:20:00', timezone='US/Eastern')
-def single_job():
-    t1 = datetime.now() 
-    read_email_from_gmail(l3a)
-    read_email_from_gmail(l4a)
-    read_email_from_gmail(l5a)
-    read_email_from_gmail(l6)
-    read_email_from_gmail(exam)
-    t2 = datetime.now()
-    print(t2-t1)
+#@sched.scheduled_job('date', id='cosc_timed_1', run_date='2021-01-11 23:20:00', timezone='US/Eastern')
+#def single_job():
+#    t1 = datetime.now() 
+#    read_email_from_gmail(l3a)
+#    read_email_from_gmail(l4a)
+#    read_email_from_gmail(l5a)
+#    read_email_from_gmail(l6)
+#    read_email_from_gmail(exam)
+#    t2 = datetime.now()
+#    print(t2-t1)
   
 # then do a series of 5 jobs with a different list object just containing 1 thing
 
-#@sched.scheduled_job('date', id='l3a', run_date='2021-01-11 23:05:00', timezone='US/Eastern')
-#def cosc102_lab3a():
-#    read_email_from_gmail("Lab 3a")
+t1 = datetime.now()
+
+@sched.scheduled_job('date', id='l3a', run_date='2021-01-11 23:35:00', timezone='US/Eastern')
+def cosc102_lab3a():
+    read_email_from_gmail(l3a)
     
-#@sched.scheduled_job('date', id='l4a', run_date='2021-01-11 23:05:00', timezone='US/Eastern')
-#def cosc102_lab4a():
-#    read_email_from_gmail("Lab 4a")
+@sched.scheduled_job('date', id='l4a', run_date='2021-01-11 23:35:00', timezone='US/Eastern')
+def cosc102_lab4a():
+    read_email_from_gmail(l4a)
     
-#@sched.scheduled_job('date', id='l5a', run_date='2021-01-11 23:05:00', timezone='US/Eastern')
-#def cosc102_lab5a():
-#    read_email_from_gmail("Lab 5a")
+@sched.scheduled_job('date', id='l5a', run_date='2021-01-11 23:35:00', timezone='US/Eastern')
+def cosc102_lab5a():
+    read_email_from_gmail(l5a)
   
-#@sched.scheduled_job('date', id='l6', run_date='2021-01-11 23:05:00', timezone='US/Eastern')
-#def cosc102_lab6():
-#    read_email_from_gmail("Lab 6")
+@sched.scheduled_job('date', id='l6', run_date='2021-01-11 23:35:00', timezone='US/Eastern')
+def cosc102_lab6():
+    read_email_from_gmail(l6)
 
-#@sched.scheduled_job('date', id='exam', run_date='2021-01-11 23:05:00', timezone='US/Eastern')
-#def cosc102_exam1():
-#    read_email_from_gmail("Final Exam")
+@sched.scheduled_job('date', id='exam', run_date='2021-01-11 23:35:00', timezone='US/Eastern')
+def cosc102_exam1():
+    read_email_from_gmail(exam)
 
+t2 = datetime.now()
+
+print(t2-t1)
 #####
 
 # COSC 102
