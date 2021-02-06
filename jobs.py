f216f33a517b0e8076d5980acd1571e1cfc69ea9
Alex Lishinski <alexlishinski@gmail.com>
2021-02-05 22:48:41 -0500

    Prepare the jobs

--- jobs.py
+++ jobs.py
@@ -8,6 +8,7 @@ import time
 import imaplib
 import email
 import os 
+from datetime import datetime, timedelta 
 
 sched = BlockingScheduler()
 
@@ -16,54 +17,17 @@ assignments_111 = ["Intro to Python Homework", "Variables and Types Homework", "
                   "Classes Homework", "Libraries Homework", "Files Homework"]
 assignments_505 = []
 assignments_list = assignments_102 + assignments_111 + assignments_505
-
-from datetime import datetime, timedelta 
   
 # Using current time 
 
-## One job   
-@sched.scheduled_job('date', id='cosc_timed_1', run_date='2021-02-03 14:42:00', timezone='US/Eastern')
-def single_job():
-    t1 = datetime.now() 
+@sched.scheduled_job('interval', id='main_esm_job', minutes=5, end_date='2021-05-06 23:59:00', timezone='US/Eastern')
+def main_job():
     read_email_from_gmail(assignments_list)
-    t2 = datetime.now()
-    print(t2-t1)
-    
-#l3a = ["Lab 3a"]
-#l4a = ["Lab 4a"]
-#l5a = ["Lab 5a"]
-#l6 = ["Lab 6"]
-#exam = ["Final Exam"]
 
-#@sched.scheduled_job('interval', id='cosc102_lab1a', minutes=1, end_date='2020-09-06 23:59:00', timezone='US/Eastern')
-  
-# 5 jobs with a different list object just containing 1 thing
-
-#t1 = datetime.now()
+# Scheduled Jobs 
 
 #@sched.scheduled_job('date', id='l3a', run_date='2021-01-11 23:35:00', timezone='US/Eastern')
 #def cosc102_lab3a():
 #    read_email_from_gmail(l3a)
-    
-#@sched.scheduled_job('date', id='l4a', run_date='2021-01-11 23:35:00', timezone='US/Eastern')
-#def cosc102_lab4a():
-#    read_email_from_gmail(l4a)
-    
-#@sched.scheduled_job('date', id='l5a', run_date='2021-01-11 23:35:00', timezone='US/Eastern')
-#def cosc102_lab5a():
-#    read_email_from_gmail(l5a)
-  
-#@sched.scheduled_job('date', id='l6', run_date='2021-01-11 23:35:00', timezone='US/Eastern')
-#def cosc102_lab6():
-#    read_email_from_gmail(l6)
-
-#@sched.scheduled_job('date', id='exam', run_date='2021-01-11 23:35:00', timezone='US/Eastern')
-#def cosc102_exam1():
-#    read_email_from_gmail(exam)
-
-#t2 = datetime.now()
-
-#print(t2-t1)
-#####
 
 sched.start()
