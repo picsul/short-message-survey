416e8c4175fae8d01281a5b307e2c8d9fc20e482
Alex Lishinski <alexlishinski@gmail.com>
2021-10-11 15:34:55 -0400

    Days and times

--- jobs.py
+++ jobs.py
@@ -13,7 +13,21 @@ picsul_number = "+18653289322"
 
 @sched.scheduled_job('date', id='bio_test', run_date='2021-10-11 10:46:00', timezone='US/Eastern')
 def bio_timed_1():
-    message_the_list(message_numbers, survey_prompt, picsul_number)      
+    message_the_list(message_numbers, survey_prompt, picsul_number)  
+    
+    
+datetimes = ["fri 10:05", "fri 11:20", "fri 12:35", "fri 15:05", "fri 15:35", "mon 12:35",
+             "mon 15:05", "tue 10:05", "tue 10:40", "tue 11:05", "tue 12:45", "tue 14:00",
+             "tue 14:25", "tue 15:40", "tue 15:50", "tue 16:20", "tue 17:20", "tue 17:36", 
+             "tue 9:00",  "thu 10:40", "thu 11:05", "thu 12:20", "thu 12:45", "thu 14:00", 
+             "thu 14:25", "thu 15:05", "thu 15:40", "thu 17:20", "thu 8:50",  "thu 9:00", 
+             "thu 9:50",  "wed 13:50", "wed 10:05", "wed 11:20", "wed 12:35", "wed 15:05", 
+             "wed 16:20", "wed 17:20", "wed 17:35", "wed 18:50"] 
+
+split_list = [x.split(" ") for x in datetimes]
+days = [el[0] for el in split_list]
+times = [el[1] for el in split_list]
+
     
 # template
 # as a weekly cron job - check the hunter version
