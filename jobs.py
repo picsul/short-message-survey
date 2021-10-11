2758c6c3d1ee43a04df6fdfeaf963ff076e8ab8c
Alex Lishinski <alexlishinski@gmail.com>
2021-10-11 10:41:24 -0400

    Update jobs.py

--- jobs.py
+++ jobs.py
@@ -11,8 +11,11 @@ survey_prompt = "Hi alex"
 
 picsul_number = "+18653289322"
 
-@sched.scheduled_job('date', id='bio_test', run_date='2021-10-11 10:16:00', timezone='US/Eastern')
+@sched.scheduled_job('date', id='bio_test', run_date='2021-10-11 10:46:00', timezone='US/Eastern')
 def bio_timed_1():
+    print(message_numbers)
+    print(survey_prompt)
+    print(picsul_number)
     message_the_list(message_numbers, survey_prompt, picsul_number)      
 
 sched.start()
