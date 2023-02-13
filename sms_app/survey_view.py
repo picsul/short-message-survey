362747a29e1f51e6a4aec8264b7e794346a4f9c8
Alex Lishinski <alexlishinski@gmail.com>
2023-02-13 11:35:04 -0500

    Went back to non-random survey

--- sms_app/survey_view.py
+++ sms_app/survey_view.py
@@ -8,40 +8,84 @@ import random
 survey_prompt = "Ready to take the BIOL 102/150/160 survey? Please respond with 'y' or 'yes' when you are ready to begin."
 sorry_message = "If you have any issues with the survey, please contact us at jmrosenberg@utk.edu."
 
-@app.route('/message')
+#@app.route('/message')
+#def sms_survey():
+#    response = MessagingResponse()
+#    
+#    from_num = request.values['From']
+#    to_num = request.values['To']
+#       
+#    # Get the message most recently sent from us
+#    messages = client.messages.list(from_=to_num, to=from_num, limit=1)
+#    message_text = messages[0].body
+#    
+#    if message_text == survey_prompt:
+#        if 'question_id' in session:
+#            del session['question_id']
+#        if 'start_time' in session:
+#            del session['start_time']
+#    
+#    if (message_text == "Thank you!" or message_text == sorry_message):
+#        resp = MessagingResponse()
+#        resp.message(sorry_message)
+#        return str(resp)
+#    else:
+#        if 'question_id' in session:
+#            response.redirect(url_for('answer',
+#                                  question_id=session['question_id']))
+#        else:
+#            survey = Survey.query.get(random.randint(1,4))
+#    
+#            if survey_error(survey, response.message):
+#                return str(response)
+#            
+#            welcome_user(survey, response.message)
+#            redirect_to_first_question(response, survey)
+#        return str(response)
+    
 def sms_survey():
     response = MessagingResponse()
     
+    now = datetime.datetime.now()
+    
+    # Get the to/from numbers from the latest request
     from_num = request.values['From']
     to_num = request.values['To']
        
     # Get the message most recently sent from us
     messages = client.messages.list(from_=to_num, to=from_num, limit=1)
     message_text = messages[0].body
-    
+        
+    # new code with 5 minute time limit, and reprompt reset
+
     if message_text == survey_prompt:
+        if 'instance_id' in session:
+            del session['instance_id']
         if 'question_id' in session:
             del session['question_id']
         if 'start_time' in session:
             del session['start_time']
-    
-    if (message_text == "Thank you!" or message_text == sorry_message):
-        resp = MessagingResponse()
-        resp.message(sorry_message)
-        return str(resp)
-    else:
-        if 'question_id' in session:
-            response.redirect(url_for('answer',
-                                  question_id=session['question_id']))
+        session['instance_id'] = messages[0].sid
+
+    if 'question_id' in session:
+        delta = now - session['start_time']
+        if delta.seconds > 300:
+            del session['start_time']
+            del session['question_id']
+            del session['instance_id']
+            response.message("The time to complete the survey has expired")
         else:
-            survey = Survey.query.get(random.randint(1,4))
-    
-            if survey_error(survey, response.message):
-                return str(response)
+            response.redirect(url_for('answer', question_id=session['question_id']))
+    else:
+        survey = Survey.query.first()
             
-            welcome_user(survey, response.message)
-            redirect_to_first_question(response, survey)
-        return str(response)
+        if survey_error(survey, response.message):
+            return str(response)
+        
+        welcome_user(survey, response.message)
+        redirect_to_first_question(response, survey)
+    return str(response)
+
 
 def redirect_to_first_question(response, survey):
     first_question = survey.questions.order_by('id').first()
@@ -50,7 +94,7 @@ def redirect_to_first_question(response, survey):
 
 
 def welcome_user(survey, send_function):
-    welcome_text = "Please respond with your agreement to the following statement on a scale from strongly disagree (1) to strongly agree (7)."
+    welcome_text = "Please respond with your perspective on the following statements."
     send_function(welcome_text)
 
 def survey_error(survey, send_function):
