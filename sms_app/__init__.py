fbeaddecd6c43021fc3eb297239511a6a51da8a7
Alex Lishinski <alexlishinski@gmail.com>
2023-02-17 21:59:38 -0500

    Update __init__.py

--- sms_app/__init__.py
+++ sms_app/__init__.py
@@ -3,16 +3,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 def prepare_app(p_db=db):
     #app.config.from_object(config_env_files[environment])
+    app = Flask(__name__)
     p_db.init_app(app)
     # load views by importing them
     from . import views
