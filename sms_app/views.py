362ce1d24ef6b7f18e4516bd0343627b67db3e5f
Alex Lishinski <alexlishinski@gmail.com>
2023-02-17 22:37:20 -0500

    Update views.py

--- sms_app/views.py
+++ sms_app/views.py
@@ -1,12 +1,13 @@
-from . import app
+#from . import app
 #from manage import app
+from flask import current_app
 from . import question_view
 from . import answer_view
 from . import survey_view
 from flask import render_template
 from .models import Question
 
-
+# change this to current app if it doesn't work
 @app.route('/')
 def root():
     questions = Question.query.all()
