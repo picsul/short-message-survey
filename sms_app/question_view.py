c69ffc3acdce0b3608c5040e78ceb38e464c7c69
Alex Lishinski <alexlishinski@gmail.com>
2023-02-18 17:07:38 -0500

    Update question_view.py

--- sms_app/question_view.py
+++ sms_app/question_view.py
@@ -1,4 +1,5 @@
-from . import app
+#from . import app
+from manage import app
 #from flask import current_app
 from twilio.twiml.messaging_response import MessagingResponse
 from .models import Question
