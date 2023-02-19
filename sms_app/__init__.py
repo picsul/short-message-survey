ff06cbcd32f8ed8ce09d6cd131b548888bdd064a
Alex Lishinski <alexlishinski@gmail.com>
2023-02-18 21:39:31 -0500

    move import views

--- sms_app/__init__.py
+++ sms_app/__init__.py
@@ -18,12 +18,13 @@ def prepare_app(p_db=db):
     app.config.from_object(config_env_files["new"])
     p_db.init_app(app)
     # load views by importing them
-    from . import views
     return app
 
 #app.run()
 app = prepare_app()
 #migrate = Migrate(app, db)
+
+from . import views
 
 
 
