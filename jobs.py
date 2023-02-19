57e3c0db76dfe54923f9be0222778b8b8de0a12d
Alex Lishinski <alexlishinski@gmail.com>
2023-02-18 22:35:07 -0500

    Update jobs.py

--- jobs.py
+++ jobs.py
@@ -2,10 +2,8 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from sms_app.send_sms import outgoing_sms, message_the_list, list_of_numbers, message_the_list_unique
 from sms_app.models import Number
 import datetime
-# never needed this before, but maybe thas the missing piece
-#from sms_app import app, db
-from flask import current_app
-
+#from flask import current_app
+from sms_app import app, db
 
 sched = BlockingScheduler()
 
@@ -13,7 +11,7 @@ survey_prompt = "Ready to take the BIOL 102 survey? Please respond with 'y' or '
 
 picsul_number = "+18653289322"
 
-datetimes = ["sat 22:28", "wed 12:20", "fri 12:20", "tue 11:00", "thu 11:00", "tue 12:35", "thu 12:35", "tue 14:10", "thu 14:10"]
+datetimes = ["sat 22:36", "wed 12:20", "fri 12:20", "tue 11:00", "thu 11:00", "tue 12:35", "thu 12:35", "tue 14:10", "thu 14:10"]
 
 split_list = [x.split(" ") for x in datetimes]
 days = [el[0] for el in split_list]
@@ -43,15 +41,16 @@ def week_check(time):
 def send_message(day, hour, minute, code):
     @sched.scheduled_job('cron', day_of_week=day, hour=hour, minute=minute, timezone='America/New_York')
     def message_job():    
-	# figure out which week we're in when job runs
-        now = datetime.datetime.today()
-        week = week_check(now)
-        # get the right people for that week
-        people = Number.query.filter(Number.week == week, Number.code.contains(code)).all()        
-        # pull out their numbers
-        message_numbers = [x.number for x in people]
-        # send the surveys
-        message_the_list(message_numbers, survey_prompt, picsul_number)  
+	with app.app_context():
+	    # figure out which week we're in when job runs
+            now = datetime.datetime.today()
+            week = week_check(now)
+            # get the right people for that week
+            people = Number.query.filter(Number.week == week, Number.code.contains(code)).all()        
+            # pull out their numbers
+            message_numbers = [x.number for x in people]
+            # send the surveys
+            message_the_list(message_numbers, survey_prompt, picsul_number)  
         
 # create the cron jobs for each unique datetime
 for i in range(0,len(datetimes)):
