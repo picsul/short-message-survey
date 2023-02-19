401a620f6961beb8f611a14eba8c803673de0b86
Alex Lishinski <alexlishinski@gmail.com>
2023-02-18 22:51:46 -0500

    cleanup

--- sms_app/survey_view.py
+++ sms_app/survey_view.py
@@ -1,5 +1,4 @@
 from . import app
-#from manage import app
 from .models import Survey
 from flask import url_for, session, request
 from sms_app.send_sms import client
@@ -9,41 +8,6 @@ import datetime
 
 survey_prompt = "Ready to take the BIOL 102 survey? Please respond with 'y' or 'yes' when you are ready to begin."
 sorry_message = "If you have any issues with the survey, please contact us at jmrosenberg@utk.edu."
-
-#@app.route('/message')
-#def sms_survey():
-#    response = MessagingResponse()
-#    
-#    from_num = request.values['From']
-#    to_num = request.values['To']
-#       
-#    # Get the message most recently sent from us
-#    messages = client.messages.list(from_=to_num, to=from_num, limit=1)
-#    message_text = messages[0].body
-#    
-#    if message_text == survey_prompt:
-#        if 'question_id' in session:
-#            del session['question_id']
-#        if 'start_time' in session:
-#            del session['start_time']
-#    
-#    if (message_text == "Thank you!" or message_text == sorry_message):
-#        resp = MessagingResponse()
-#        resp.message(sorry_message)
-#        return str(resp)
-#    else:
-#        if 'question_id' in session:
-#            response.redirect(url_for('answer',
-#                                  question_id=session['question_id']))
-#        else:
-#            survey = Survey.query.get(random.randint(1,4))
-#    
-#            if survey_error(survey, response.message):
-#                return str(response)
-#            
-#            welcome_user(survey, response.message)
-#            redirect_to_first_question(response, survey)
-#        return str(response)
     
 @app.route('/message')
 def sms_survey():
