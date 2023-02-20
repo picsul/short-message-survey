b751eb9c20163501d12880ff16bd18cf3ea652a5
Alex Lishinski <alexlishinski@gmail.com>
2023-02-19 21:46:42 -0500

    Update jobs.py

--- jobs.py
+++ jobs.py
@@ -11,7 +11,7 @@ survey_prompt = "Ready to take the BIOL 102 survey? Please respond with 'y' or '
 
 picsul_number = "+18652639199"
 
-datetimes = ["sun 21:34", "wed 12:20", "fri 12:20", "tue 11:00", "thu 11:00", "tue 12:35", "thu 12:35", "tue 14:10", "thu 14:10"]
+datetimes = ["mon 02:48", "wed 12:20", "fri 12:20", "tue 11:00", "thu 11:00", "tue 12:35", "thu 12:35", "tue 14:10", "thu 14:10"]
 
 split_list = [x.split(" ") for x in datetimes]
 days = [el[0] for el in split_list]
