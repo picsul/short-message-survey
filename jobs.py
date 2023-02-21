77c21e3a2b7f1e2ac8df515675ce0800bfb6be30
Alex Lishinski <alexlishinski@gmail.com>
2023-02-20 21:52:13 -0500

    Update jobs.py

--- jobs.py
+++ jobs.py
@@ -8,10 +8,9 @@ sched = BlockingScheduler()
 
 survey_prompt = "Ready to take the BIOL 102 survey? Please respond with 'y' or 'yes' when you are ready to begin."
 
-
 picsul_number = "+18652639199"
 
-datetimes = ["mon 21:49", "wed 12:20", "fri 12:20", "tue 11:00", "thu 11:00", "tue 12:35", "thu 12:35", "tue 14:10", "thu 14:10"]
+datetimes = ["mon 21:54", "wed 12:20", "fri 12:20", "tue 11:00", "thu 11:00", "tue 12:35", "thu 12:35", "tue 14:10", "thu 14:10"]
 
 split_list = [x.split(" ") for x in datetimes]
 days = [el[0] for el in split_list]
@@ -37,37 +36,6 @@ def week_check(time):
             diffs.append(0)
     return str(sum(diffs))
 
-#jobs = []
-        
-#def message_job(code):    
-#    with app.app_context():
-#        # figure out which week we're in when job runs
-#        now = datetime.datetime.today()
-#        week = week_check(now)
-#        # get the right people for that week
-#        people = Number.query.filter(Number.week == week, Number.code.contains(code)).all()        
-#        # pull out their numbers
-#        message_numbers = [x.number for x in people]
-#        # send the surveys
-#        message_the_list(message_numbers, survey_prompt, picsul_number)      
-       
-# create the cron jobs for each unique datetime
-#for i in range(0,len(datetimes)):
-#    job = sched.add_job(message_job, 'cron', day_of_week=days[i], hour=hours[i], minute=mins[i], args=[codes[i]], timezone='America/New_York')
-#    jobs.append(job)
-
-# create an empty list to hold job references
-
-# access job attributes using the job references
-#for job in jobs:
-#    job_id = job.id
-#    job_name = job.name
-#    job_next_run_time = job.trigger.next_run_time(None, job)
-
-#    print(f'Job ID: {job_id}')
-#    print(f'Job name: {job_name}')
-#    print(f'Next run time: {job_next_run_time}')
-    
 # create a cron job to send a message to a subset of people dependent on the week
 def send_message(day, hour, minute, code):
     @sched.scheduled_job('cron', day_of_week=day, hour=hour, minute=minute, timezone='America/New_York')
