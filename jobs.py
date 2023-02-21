54ac0dbd19117cd85fa353144914eab16a02b80e
Alex Lishinski <alexlishinski@gmail.com>
2023-02-20 22:14:33 -0500

    Update jobs.py

--- jobs.py
+++ jobs.py
@@ -10,7 +10,7 @@ survey_prompt = "Ready to take the BIOL 102 survey? Please respond with 'y' or '
 
 picsul_number = "+18652639199"
 
-datetimes = ["mon 22:06", "wed 12:20", "fri 12:20", "tue 11:00", "thu 11:00", "tue 12:35", "thu 12:35", "tue 14:10", "thu 14:10"]
+datetimes = ["sun 22:17", "wed 12:20", "fri 12:20", "tue 11:00", "thu 11:00", "tue 12:35", "thu 12:35", "tue 14:10", "thu 14:10"]
 
 split_list = [x.split(" ") for x in datetimes]
 days = [el[0] for el in split_list]
@@ -40,19 +40,19 @@ def week_check(time):
 def send_message(day, hour, minute, code):
     @sched.scheduled_job('cron', day_of_week=day, hour=hour, minute=minute, timezone='America/New_York')
     def message_job():    
-	# figure out which week we're in when job runs
-        now = datetime.datetime.today()
-        week = week_check(now)
-        # get the right people for that week
-        people = Number.query.filter(Number.week == week, Number.code.contains(code)).all()        
-        # pull out their numbers
-        message_numbers = [x.number for x in people]
-        # send the surveys
-        message_the_list(message_numbers, survey_prompt, picsul_number)  
+        with app.app_context():
+            # figure out which week we're in when job runs
+            now = datetime.datetime.today()
+            week = week_check(now)
+            # get the right people for that week
+            people = Number.query.filter(Number.week == week, Number.code.contains(code)).all()        
+            # pull out their numbers
+            message_numbers = [x.number for x in people]
+            # send the surveys
+            message_the_list(message_numbers, survey_prompt, picsul_number)  
         
 # create the cron jobs for each unique datetime
 for i in range(0,len(datetimes)):
     send_message(days[i], hours[i], mins[i], codes[i])
 
-# start the scheduler
 sched.start()
