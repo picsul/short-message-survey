e6b6c4686c50bb194ab0576f170cf73728f98960
Alex Lishinski <alexlishinski@gmail.com>
2023-02-22 20:16:51 -0500

    update the survey view

--- sms_app/survey_view.py
+++ sms_app/survey_view.py
@@ -34,47 +34,51 @@ def sms_survey():
         if 'start_time' in session:
             del session['start_time']
         #session['instance_id'] = messages[0].sid
-        
-    #print(now)
-    #print(session)
-
-    if 'question_id' in session:
-        delta = now - session['start_time']
-        if delta.seconds > 300:
-            del session['start_time']
-            del session['question_id']
-            #del session['instance_id']
-            response.message("The time to complete the survey has expired")
-        else:
-            response.redirect(url_for('answer', question_id=session['question_id']))
-    else:
-        survey = Survey.query.first()
-            
-        if survey_error(survey, response.message):
-            return str(response)
-        
-        welcome_user(survey, response.message)
-        redirect_to_first_question(response, survey)
-    return str(response)
 
 
-    #if (message_text == "Thank you!" or message_text == sorry_message):
-    #    resp = MessagingResponse()
-    #    resp.message(sorry_message)
-    #    return str(resp)
-    #else:
-    #    if 'question_id' in session:
-    #        response.redirect(url_for('answer',
-    #                              question_id=session['question_id']))
-    #    else:
-    #        survey = Survey.query.first()
-   # 
-   #         if survey_error(survey, response.message):
-    #            return str(response)
-    #        
-    #        welcome_user(survey, response.message)
-    #        redirect_to_first_question(response, survey)
-    #    return str(response)
+    # if 'question_id' in session:
+    #     delta = now - session['start_time']
+    #     if delta.seconds > 300:
+    #         del session['start_time']
+    #         del session['question_id']
+    #         #del session['instance_id']
+    #         response.message("The time to complete the survey has expired")
+    #     else:
+    #         response.redirect(url_for('answer', question_id=session['question_id']))
+    # else:
+    #     survey = Survey.query.first()
+    #         
+    #     if survey_error(survey, response.message):
+    #         return str(response)
+    #     
+    #     welcome_user(survey, response.message)
+    #     redirect_to_first_question(response, survey)
+    # return str(response)
+
+
+   if (message_text == "Thank you!" or message_text == sorry_message):
+      resp = MessagingResponse()
+      resp.message(sorry_message)
+      return str(resp)
+   else:
+      if 'question_id' in session:
+          delta = now - session['start_time']
+          if delta.seconds > 300:
+              del session['start_time']
+              del session['question_id']
+              #del session['instance_id']
+              response.message("The time to complete the survey has expired")
+          else:
+              response.redirect(url_for('answer', question_id=session['question_id']))
+      else:
+          survey = Survey.query.first()
+
+           if survey_error(survey, response.message):
+              return str(response)
+
+          welcome_user(survey, response.message)
+          redirect_to_first_question(response, survey)
+      return str(response)
 
 
 def redirect_to_first_question(response, survey):
