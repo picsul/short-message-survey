1c86bb7768e22694bd9ea9fca84f987682f2115a
Alex Lishinski <alexlishinski@gmail.com>
2023-02-25 23:05:07 -0500

    fjdos

--- sms_app/send_sms.py
+++ sms_app/send_sms.py
@@ -23,7 +23,7 @@ def outgoing_sms(number, body, out_num):
 def message_the_list(num_list, body, from_num):
     for num in num_list:
         try:
-            outgoing_sms(num, body, from_num)
+            sms = outgoing_sms(num, body, from_num)
             db.save(Instance(sid = sms, assign = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")))
         except twilio.base.exceptions.TwilioRestException:
             pass   
