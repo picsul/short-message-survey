703c473157bbcead6cf772ef182c9f0fac3885cd
Alex Lishinski <alexlishinski@gmail.com>
2023-03-08 21:49:55 -0500

    jodi

--- jobs.py
+++ jobs.py
@@ -22,7 +22,7 @@ phone_number = confi['phone_number']
 # with open("config.toml", "rb") as f:
 #     confi = tomllib.load(f)
 
-datetimes = ["thu 22:54", "wed 12:20", "fri 12:20", "tue 11:00", "thu 11:00", "tue 12:35", "thu 12:35", "tue 14:10", "thu 14:10"]
+datetimes = ["wed 21:52", "wed 12:20", "fri 12:20", "tue 11:00", "thu 11:00", "tue 12:35", "thu 12:35", "tue 14:10", "thu 14:10"]
 
 split_list = [x.split(" ") for x in datetimes]
 days = [el[0] for el in split_list]
@@ -35,7 +35,7 @@ mins = [el[1] for el in split_times]
 codes = ["M1220", "W1220", "F1220", "T1100", "R1100", "T1235", "R1235", "T1410", "R1410"]
 
 # create list of beginning of week times to index the weeks
-base = datetime.datetime(2023, 2, 26, 1,1,1)
+base = datetime.datetime(2023, 3, 5, 1,1,1)
 date_list = [base + datetime.timedelta(weeks=x) for x in range(13)]
 
 # count which week we are in by comparing a time to the beginning of week times list
