24b1d5f611002629267f7419b78a5ce2b5933cf2
Alex Lishinski <alexlishinski@gmail.com>
2023-03-30 22:48:03 -0400

    jfod

--- jobs.py
+++ jobs.py
@@ -22,7 +22,7 @@ phone_number = confi['phone_number']
 # with open("config.toml", "rb") as f:
 #     confi = tomllib.load(f)
 
-datetimes = ["thu 22:32", "wed 12:20", "fri 12:20", "tue 11:00", "thu 11:00", "tue 12:35", "thu 12:35", "tue 14:10", "thu 14:10"]
+datetimes = ["thu 22:49", "wed 12:20", "fri 12:20", "tue 11:00", "thu 11:00", "tue 12:35", "thu 12:35", "tue 14:10", "thu 14:10"]
 
 split_list = [x.split(" ") for x in datetimes]
 days = [el[0] for el in split_list]
