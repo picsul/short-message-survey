7129d13398837e32462424a730045f74fda72376
Alex Lishinski <alexlishinski@gmail.com>
2023-05-19 10:40:33 -0400

    Update jobs.py

--- jobs.py
+++ jobs.py
@@ -19,7 +19,7 @@ sched = BlockingScheduler()
 survey_prompt = confi['survey_prompt']
 phone_number = confi['phone_number']
 
-datetimes = ["fri 10:39", "wed 12:20", "fri 12:20", "tue 11:00", "thu 11:00", "tue 12:35", "thu 12:35", "tue 14:10", "thu 14:10"]
+datetimes = ["fri 10:42", "wed 12:20", "fri 12:20", "tue 11:00", "thu 11:00", "tue 12:35", "thu 12:35", "tue 14:10", "thu 14:10"]
 
 split_list = [x.split(" ") for x in datetimes]
 days = [el[0] for el in split_list]
@@ -46,19 +46,19 @@ def week_check(time):
     return str(sum(diffs))
 
 # create a cron job to send a message to a subset of people dependent on the week
-#def send_message(day, hour, minute, code):
-#    @sched.scheduled_job('cron', day_of_week=day, hour=hour, minute=minute, timezone='America/New_York')
-#    def message_job():    
-#        with app.app_context():
-#            # figure out which week we're in when job runs
-#            now = datetime.datetime.today()
-#            week = week_check(now)
-#            # get the right people for that week
-#            people = Number.query.filter(Number.week == week, Number.code.contains(code)).all()        
-#            # pull out their numbers
-#            message_numbers = [x.number for x in people]
-#            # send the surveys
-#            message_the_list(message_numbers, survey_prompt, phone_number)  
+def send_message(day, hour, minute, code):
+    @sched.scheduled_job('cron', day_of_week=day, hour=hour, minute=minute, timezone='America/New_York')
+    def message_job():    
+        with app.app_context():
+            # figure out which week we're in when job runs
+            now = datetime.datetime.today()
+            week = week_check(now)
+            # get the right people for that week
+            people = Number.query.filter(Number.week == week, Number.code.contains(code)).all()        
+            # pull out their numbers
+            message_numbers = [x.number for x in people]
+            # send the surveys
+            message_the_list(message_numbers, survey_prompt, phone_number)  
         
 # create the cron jobs for each unique datetime
 #for i in range(0,len(datetimes)):
