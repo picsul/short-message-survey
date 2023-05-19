65e848731f90718df7d98cb99fdcb25096ac1fe1
Alex Lishinski <alexlishinski@gmail.com>
2023-05-19 12:06:10 -0400

    Update __init__.py

--- sms_app/__init__.py
+++ sms_app/__init__.py
@@ -29,8 +29,8 @@ from .question_view import question_bp
 app.register_blueprint(question_bp)
 from .views import bp
 app.register_blueprint(bp)
-#from .survey_view import survey_bp
-#app.register_blueprint(survey_bp)
+from .survey_view import survey_bp
+app.register_blueprint(survey_bp)
 from .answer_view import answer_bp
 app.register_blueprint(answer_bp)
 
