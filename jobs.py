f12b48944f6d97fd44699b591250aee33133dc29
Alex Lishinski <alexlishinski@gmail.com>
2023-05-18 21:58:27 -0400

    Update jobs.py

--- jobs.py
+++ jobs.py
@@ -1,9 +1,9 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
-from sms_app.send_sms import outgoing_sms, message_the_list, message_the_list_unique
-from sms_app.models import Number
+#from sms_app.send_sms import outgoing_sms, message_the_list, message_the_list_unique
+#from sms_app.models import Number
 import datetime
-from sms_app import app, db, confi
+#from sms_app import app, db, confi
 import os
 import tomllib
 
@@ -22,31 +22,31 @@ sched = BlockingScheduler()
 #with open("config.toml", "rb") as f:
 #    confi = tomllib.load(f)
 
-datetimes = ["thu 20:27", "wed 12:20", "fri 12:20", "tue 11:00", "thu 11:00", "tue 12:35", "thu 12:35", "tue 14:10", "thu 14:10"]
+#datetimes = ["thu 20:27", "wed 12:20", "fri 12:20", "tue 11:00", "thu 11:00", "tue 12:35", "thu 12:35", "tue 14:10", "thu 14:10"]
 
-split_list = [x.split(" ") for x in datetimes]
-days = [el[0] for el in split_list]
+#split_list = [x.split(" ") for x in datetimes]
+#days = [el[0] for el in split_list]
 
-times = [el[1] for el in split_list]
-split_times = [x.split(":") for x in times]
-hours = [el[0] for el in split_times]
-mins = [el[1] for el in split_times]
+#times = [el[1] for el in split_list]
+#split_times = [x.split(":") for x in times]
+#hours = [el[0] for el in split_times]
+#mins = [el[1] for el in split_times]
 
-codes = ["M1220", "W1220", "F1220", "T1100", "R1100", "T1235", "R1235", "T1410", "R1410"]
+#codes = ["M1220", "W1220", "F1220", "T1100", "R1100", "T1235", "R1235", "T1410", "R1410"]
 
 # create list of beginning of week times to index the weeks
-base = datetime.datetime(2023, 5, 8, 1,1,1)
-date_list = [base + datetime.timedelta(weeks=x) for x in range(13)]
+#base = datetime.datetime(2023, 5, 8, 1,1,1)
+#date_list = [base + datetime.timedelta(weeks=x) for x in range(13)]
 
 # count which week we are in by comparing a time to the beginning of week times list
-def week_check(time):
-    diffs = []
-    for i in range(0, len(date_list)):
-        if time > date_list[i]:
-            diffs.append(1)
-        else:
-            diffs.append(0)
-    return str(sum(diffs))
+#def week_check(time):
+#    diffs = []
+#    for i in range(0, len(date_list)):
+#        if time > date_list[i]:
+#            diffs.append(1)
+#        else:
+#            diffs.append(0)
+#    return str(sum(diffs))
 
 # create a cron job to send a message to a subset of people dependent on the week
 #def send_message(day, hour, minute, code):
