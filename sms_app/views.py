4e4da694d77c48f0d0d7c58166a9f3f8c39992f4
Alex Lishinski <alexlishinski@gmail.com>
2023-05-19 09:40:33 -0400

    Update views.py

--- sms_app/views.py
+++ sms_app/views.py
@@ -1,12 +1,14 @@
-from . import app
+#from . import app
 #from . import question_view
 #from . import answer_view
 #from . import survey_view
-from flask import render_template
+from flask import render_template, Blueprint
 from .models import Question
 
+bp = Blueprint('survey', __name__)
+
 # change this to current app if it doesn't work
-@app.route('/')
+@bp.route('/')
 def root():
     questions = Question.query.all()
     return render_template('index.html', questions=questions)
