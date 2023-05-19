418647b0923f1feea7815648f18cb17adcb766fe
Alex Lishinski <alexlishinski@gmail.com>
2023-05-19 10:43:02 -0400

    Update jobs.py

--- jobs.py
+++ jobs.py
@@ -19,7 +19,7 @@ sched = BlockingScheduler()
 survey_prompt = confi['survey_prompt']
 phone_number = confi['phone_number']
 
-datetimes = ["fri 10:42", "wed 12:20", "fri 12:20", "tue 11:00", "thu 11:00", "tue 12:35", "thu 12:35", "tue 14:10", "thu 14:10"]
+datetimes = ["fri 10:44", "wed 12:20", "fri 12:20", "tue 11:00", "thu 11:00", "tue 12:35", "thu 12:35", "tue 14:10", "thu 14:10"]
 
 split_list = [x.split(" ") for x in datetimes]
 days = [el[0] for el in split_list]
@@ -61,7 +61,7 @@ def send_message(day, hour, minute, code):
             message_the_list(message_numbers, survey_prompt, phone_number)  
         
 # create the cron jobs for each unique datetime
-#for i in range(0,len(datetimes)):
-#    send_message(days[i], hours[i], mins[i], codes[i])
+for i in range(0,len(datetimes)):
+    send_message(days[i], hours[i], mins[i], codes[i])
 
 sched.start()
